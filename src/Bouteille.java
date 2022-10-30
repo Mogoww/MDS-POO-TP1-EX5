59d6368c23f17b61fb4ed41dc476ac410dc92b66
Gauthier <gauthier.mauche@gmail.com>
2022-10-30 18:09:00 +0100

    add attribute and construct

--- src/Bouteille.java
+++ src/Bouteille.java
@@ -1,2 +1,12 @@
 public class Bouteille {
+
+    // attributs
+    private int quantite;
+    private boolean estOuverte;
+
+    // constructeur
+    public Bouteille(int quantite){
+        this.quantite = quantite;
+        this.estOuverte = false;
+    }
 }
